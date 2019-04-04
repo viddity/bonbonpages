74bbd9b01734ff631437a92534b3b7dd7f1cee17
Verena Brodbeck <verenabrodbeck@gmx.de>
2019-04-04 14:59:55 +0200

    CHG: Minor adaption of gatsby starter setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Home" keywords={[`Verena Brodbeck`, `bonbonpages`, `react`, `gatsby`]} />
+    <h1>Hi</h1>
+    <p>
+      This needs some text, I know.
+    </p>
+    <Link to="/about/">About</Link>
   </Layout>
 )
 
