53bbda413fe2c4755c2a767d9c0b92212569098a
Verena Brodbeck <verenabrodbeck@gmx.de>
2019-04-25 15:14:24 +0200

    FIX: Remove unused import

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql, Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
