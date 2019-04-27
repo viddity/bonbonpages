3712568836aa9d6772f7e404c3e368cec2c6f84c
Verena Brodbeck <verenabrodbeck@gmx.de>
2019-04-27 10:28:50 +0200

    CHG: Add test impressum

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,9 +6,25 @@ import SEO from "../components/seo"
 const Impressum = () => (
   <Layout>
     <SEO title="bonbonpages Impressum" />
-    <h1>Hi from the Impressum page</h1>
-    <p>More soon</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="cf" style={{
+      background: `url(http://mrmrs.github.io/photos/12.jpg) no-repeat center center fixed`,
+      backgroundSize: `cover`,
+    }}>
+      <div className="fl pa3 pa4-ns bg-white black-70 measure-narrow f3 times">
+        <header className="bb b--black-70 pv4">
+          <h3 className="f2 fw7 ttu tracked lh-title mt0 mb3 avenir">Impressum</h3>
+        </header>
+        <section className="pt5 pb4">
+          <p className="times lh-copy measure f4 mt0">
+            Verena Brodbeck<br/>Bottenhorner Weg 2<br/>60489 Frankfurt
+          </p>
+          <p className="times lh-copy measure f4 mt0">
+            UstId: <br/><a className="no-underline gray dim" href="mailto:kontakt@bonbonpages.de">Mail me</a>
+          </p>
+        </section>
+      </div>
+
+    </div>
   </Layout>
 )
 
