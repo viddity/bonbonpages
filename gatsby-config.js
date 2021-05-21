ed644eeb5f5299aad5c4f974bccd5ff7eecbf171
Verena Brodbeck <verenabrodbeck@gmx.de>
2021-05-21 12:07:20 +0200

    chore: upgrade packages and fix gatsby config plugin paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@viddity`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {resolve: `gatsby-plugin-react-helmet`},
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +13,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {resolve: `gatsby-transformer-sharp`},
+    {resolve: `gatsby-plugin-sharp`},
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -36,7 +36,7 @@ module.exports = {
         noIframeBorder: true //Optional: Disable insertion of <style> border: 0
       }
     },
-    `gatsby-plugin-sass`,
+    {resolve: `gatsby-plugin-sass`},
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
